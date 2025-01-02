8427634ac585997f1c282e8666d5720ee6034e7c
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-01-02 11:00:43 -0700

    working on DeliveryScreen but still not fixed

--- src/features/delivery/DeliveryScreen.tsx
+++ src/features/delivery/DeliveryScreen.tsx
@@ -1,10 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Platform, View} from 'react-native'
+import React, { FC } from 'react'
+import { useStyles } from 'react-native-unistyles'
+import { homeStyles } from '@unistyles/homeStyles'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import Animated, { Extrapolate, interpolate, useAnimatedStyle } from 'react-native-reanimated'
+import { useSharedState } from '@features/tabs/SharedContext'
 
-const DeliveryScreen = () => {
+const DeliveryScreen:FC = () => {
+  const insets=useSafeAreaInsets()
+  const {styles}=useStyles(homeStyles)
+  const {scrollGlobal}=useSharedState()
+ const backgroundColorChanges=useAnimatedStyle(()=>{
+  const opacity=interpolate(scrollGlobal.value,[0,50],[0,1])
+  return {
+    backgroundColor:'rgba(255,255,255,${opacity})'
+  }
+
+ })
+
+  const moveUpStyle=useAnimatedStyle(()=>{
+    const translateY=interpolate(
+    scrollGlobal.value,
+    [0,50],
+    [0,-50],
+    Extrapolate.CLAMP
+  )
+  return {
+    transform:[{translateY}]
+  }
+  })
+  const moveUpStyleNotExtrapolate=useAnimatedStyle(()=>{
+    const translateY=interpolate(
+    scrollGlobal.value,
+    [0,50],
+    [0,-50],
+  )
+  return {
+    transform:[{translateY}]
+  }
+  })
+  
+
   return (
-    <View>
-      <Text>DeliveryScreen</Text>
+    <View style={styles.container}>
+      <View style={{height:Platform.OS==='android'?insets.top:0}}/>
+      <Animated.View style={[moveUpStyle]}>
+      <Animated.View style={[moveUpStyleNotExtrapolate]}>
+
+      </Animated.View>
+      <Animated.View style={[backgroundColorChanges,styles.topHeader]}>
+
+      </Animated.View>
+      </Animated.View>
+
     </View>
   )
 }
